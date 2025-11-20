d035c45b15337353cb16d7f0c031f781abbefae6
Lajana <np01cp4a250144@islingtoncollege.edu.np>
2025-11-20 10:31:05 +0545

    Println

---
+++ github_1.java
@@ -0,0 +1,13 @@
+
+/**
+ * Workshop_3
+ *
+ * @author Lajana
+ * @version v1.0
+ */
+public class github_1
+{
+    public static void main(String[] args){
+    System.out.println("First time using github");
+    }
+}
